12f56e4e19e99a91df4ef6f2fb4d238ba1fb2c50
Abiola Ajibola <abiolaajibola955@gmail.com>
2022-06-04 00:24:03 +0100

    First commitment
    First commit

---
+++ First Attempt/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+$fname = $_POST["fname"]; 
+echo $fname;
+ $lname = $_POST["lname"]; 
+ echo $lname;
+ $uname = $_POST["uname"];
+ echo $uname; 
+ $pword= $_POST["pword"]; 
+ echo $pword;
+ $pnumber = $_POST["pnumber"];
+ echo $pnumber;
+ $gender = $_POST["gender"]; 
+ echo $gender;
+ $nline = $_POST["newline"];
+ echo $nline;
+ ?><br>
+
+</body>
+</html>
