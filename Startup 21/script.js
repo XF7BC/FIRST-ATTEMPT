4c2262c6b48f5b9092eb5d3c2be758a5ed41eceb
Abiola Ajibola <abiolaajibola955@gmail.com>
2022-06-12 13:49:52 +0100

    correction

--- Startup 21/script.js
+++
@@ -1,9 +0,0 @@
-function validateForm() {
-let x = document.forms["form"]["newsletter_subscription"].value;
-    if (x == "") {
-      alert("E-mail must be filled out");
-      return false;
-    }
-    
-}
-
